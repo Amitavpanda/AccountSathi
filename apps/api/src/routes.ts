ab37c086941f38c96090e15bdb3fdc83bbe71d98
Amitav Panda <amitavpanda@Amitav-ka-MacBook-Pro.local>
2024-10-26 19:46:34 +0530

    changes in the routes to fix cors issue

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -19,7 +19,10 @@ import { purchaseDataDurationHandler } from "./contollers/purchaseDataDuration.c
 function routes(app : Express){
     app.get('/healthcheck', (req : Request, res : Response)  => res.sendStatus(200));
 
-
+    app.use((req: Request, res: Response, next) => {
+        console.log("CORS Headers:", res.getHeaders()); // Log headers for each route
+        next();
+    });
     app.post('/getPurchasesDataDuration', validate(getPurchaseDataDurationSchema), purchaseDataDurationHandler);
     app.post('/getSalesDataDuration', validate(getSalesDataDurationSchema), salesDataDurationHandler);
     
